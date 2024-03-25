e112b6971fae7e3eb70f20140b06949895291db1
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-03-25 13:25:23 -0300

    firts-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
